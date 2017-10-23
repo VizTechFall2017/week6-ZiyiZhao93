e903312d850089d47142758b71b4d710f0fbd920
ZiyiZhao93 <31828737+ZiyiZhao93@users.noreply.github.com>
2017-10-23 00:14:18 -0400

    10.23
    have problem on rect

--- barChart_exit/index.js
+++ barChart_exit/index.js
@@ -10,74 +10,64 @@ var svg = d3.select('svg')
     .append('g')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
-//these are the size that the axes will be on the screen; set the domain values after the data loads.
 var scaleX = d3.scaleBand().rangeRound([0, 600]).padding(0.1);
 var scaleY = d3.scaleLinear().range([400, 0]);
 
 
-//import the data from the .csv file
-d3.csv('./countryData.csv', function(dataIn){
+
+d3.csv('./SubwayActivityServices.csv', function(dataIn){
+
+    console.log(dataIn);
 
     nestedData = d3.nest()
-        .key(function(d){return d.year})
+        .key(function(d){return d.genderCode})
         .entries(dataIn);
 
-    var loadData = nestedData.filter(function(d){return d.key == '1987'})[0].values;
+    var total = nestedData.filter(function(d){return d.key == '0'})[0].values;
 
-    scaleX.domain(loadData.map(function(d){return d.countryCode;}));
-    scaleY.domain([0, d3.max(loadData.map(function(d){return +d.totalPop}))]);
+    scaleX.domain(total.map(function(d){return d.things;}));
+    scaleY.domain([0,100]);
 
-    // Add the x Axis
     svg.append("g")
         .attr('transform','translate(0,400)')  //move the x axis from the top of the y axis to the bottom
         .call(d3.axisBottom(scaleX));
 
     svg.append("g")
-        .attr('class','yaxis')
+        .attr('class','percent')
         .call(d3.axisLeft(scaleY));
 
-/*
     svg.append('text')
-        .text('Weekly income by age and gender')
-        .attr('transform','translate(300, -20)')
+        .text('What Do you Normally Do on the Subway?')
+        .attr('transform','translate(300, -40)')
         .style('text-anchor','middle');
 
     svg.append('text')
-        .text('age group')
-        .attr('transform','translate(260, 440)');
+        .text('percent')
+        .attr('transform', 'translate(-50,-10)');
 
-    svg.append('text')
-        .text('weekly income')
-        .attr('transform', 'translate(-50,250)rotate(270)');
-
-        */
-
-    //bind the data to the d3 selection, but don't draw it yet
     svg.selectAll('rect')
-        .data(loadData)
+        .data(total)
         .enter()
         .append('rect')
         .attr('class','bars')
         .attr('fill', "slategray");
 
-    //call the drawPoints function below, and hand it the data2016 variable with the 2016 object array in it
-    drawPoints(loadData);
+    drawPoints(total);
+
 
 });
 
-//this function draws the actual data points as circles. It's split from the enter() command because we want to run it many times
-//without adding more circles each time.
 function drawPoints(pointData){
 
     scaleY.domain([0, d3.max(pointData.map(function(d){return +d.totalPop}))]);
 
-    svg.selectAll('.yaxis')
+    svg.selectAll('.percent')
         .call(d3.axisLeft(scaleY));
 
     svg.selectAll('rect')
         .data(pointData)
         .attr('x',function(d){
-            return scaleX(d.countryCode);
+            return scaleX(d.things);
         })
         .attr('y',function(d){
             return scaleY(d.totalPop);
@@ -86,21 +76,21 @@ function drawPoints(pointData){
             return scaleX.bandwidth();
         })
         .attr('height',function(d){
-            return 400 - scaleY(d.totalPop);  //400 is the beginning domain value of the y axis, set above
+            return 400 - scaleY(d.totalPop);
         });
 
 }
 
 
-function updateData(selectedYear){
-    return nestedData.filter(function(d){return d.key == selectedYear})[0].values;
+function updateData(selectedGender){
+    return nestedData.filter(function(d){return d.key == selectedGender})[0].values;
 }
 
 
-//this function runs when the HTML slider is moved
 function sliderMoved(value){
 
     newData = updateData(value);
     drawPoints(newData);
 
 }
+
